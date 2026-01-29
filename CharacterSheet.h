a619d8c30cc41beb07db3ee8a9dbc268629f3b10
Tobias Fajks <toby.fajks@gmail.com>
2026-01-30 00:17:54 +0100

    Implemented attribute picking and automatic attribute adding through the specie choice

--- CharacterSheet.h
+++ CharacterSheet.h
@@ -1,34 +1,59 @@
 //CharacterSheet.h
 #include <iostream>
 #include <fstream>
+#include <chrono>
 enum SPECIE {human, halfelf, elf, tiefling};
 enum CLASS {palladin, wizard};
+struct attributes
+{
+	int strength = 0;
+	int dexterity = 0;
+	int constitution = 0;
+	int intelligence = 0;
+	int wisdom = 0;
+	int charisma = 0;
+};
 class Character 
 {
 public:
+	
+	int free_attribute_points = 0;
 	SPECIE _specie;
 	CLASS _class;
+	attributes _attr;
 	std::string _name;
-	Character(std::string name, SPECIE specie, CLASS char_class) 
+	Character(std::string name, SPECIE specie, CLASS char_class, attributes attr) 
 	{
 		_name = name;
 		_specie = specie;
 		_class = char_class;
+		_attr = attr;
 	}
 	std::string getSpecie()
 	{
 		switch (_specie)
 		{
 		case human:
+			_attr.strength++;
+			_attr.dexterity++;
+			_attr.constitution++;
+			_attr.intelligence++;
+			_attr.wisdom++;
+			_attr.charisma++;
 			return "human";
 			break;
 		case halfelf:
+			_attr.charisma += 2;
+			free_attribute_points = 2;
 			return "half - elf";
 			break;
 		case elf:
+			_attr.dexterity+= 2;
 			return "elf";
 			break;
 		case tiefling:
+			_attr.intelligence += 1;
+			_attr.charisma += 2;
 			return "tiefling";
 			break;
 		}
@@ -46,7 +71,17 @@ public:
 		}
 	}
 };
-void write_character_informations(Character new_character) 
+int attribute_pick[6] = {};
+void getAttributes()
+{
+	srand(time(NULL));
+	for (int i = 0; i < 6; i++)
+	{
+		attribute_pick[i] = (rand() % 14) + 4;
+		std::cout << "[" << i + 1 << "]" << " " << attribute_pick[i] << std::endl;
+	}
+}
+void enter_character_informations(Character new_character) 
 {
 	std::ofstream file;
 	file.open("character_save.txt");
@@ -54,5 +89,11 @@ void write_character_informations(Character new_character)
 	character_save << new_character._name << std::endl;
 	character_save << new_character.getSpecie() << std::endl;
 	character_save << new_character.getClass() << std::endl;
+	character_save << new_character._attr.strength << std::endl;
+	character_save << new_character._attr.dexterity << std::endl;
+	character_save << new_character._attr.constitution << std::endl;
+	character_save << new_character._attr.intelligence << std::endl;
+	character_save << new_character._attr.wisdom << std::endl;
+	character_save << new_character._attr.charisma << std::endl;
 	file.close();
 }
--- DnD.cpp
+++ DnD.cpp
@@ -2,19 +2,62 @@
 #include <string>
 #include <cstdlib>
 #include "CharacterSheet.h"
-int main()
+void create_character() 
 {
+    attributes attr;
     std::string character_name;
     int SpecieInput;
+    int AttrInput;
     int ClassInput;
     std::cout << "enter character's name:\n";
     getline(std::cin, character_name);
     std::cout << "[1]human\n[2]half-elf\n[3]elf\n[4]tiefling\n";
     std::cout << "what's your character's race?\n";
     std::cin >> SpecieInput;
+    getAttributes();
+    for (int i = 0; i < 6; i++) 
+    {
+        switch (i) 
+        {
+        case 0:
+            std::cout << "pick number for Strength: ";
+            std::cin >> AttrInput;
+            attr.strength += attribute_pick[AttrInput-1];
+            break;
+        case 1:
+            std::cout << "pick number for Dexterity: ";
+            std::cin >> AttrInput;
+            attr.dexterity += attribute_pick[AttrInput-1];
+            break;
+        case 2:
+            std::cout << "pick number for Constitution: ";
+            std::cin >> AttrInput;
+            attr.constitution += attribute_pick[AttrInput-1];
+            break;
+        case 3:
+            std::cout << "pick number for Intelligence: ";
+            std::cin >> AttrInput;
+            attr.intelligence += attribute_pick[AttrInput-1];
+            break;
+        case 4:
+            std::cout << "pick number for Wisdom: ";
+            std::cin >> AttrInput;
+            attr.wisdom += attribute_pick[AttrInput-1];
+            break;
+        case 5:
+            std::cout << "pick number for Charisma: ";
+            std::cin >> AttrInput;
+            attr.charisma += attribute_pick[AttrInput-1];
+            break;
+        }
+    }
     std::cout << "[1]palladin\n[2]wizard\n";
     std::cout << "what's your character's class?\n";
     std::cin >> ClassInput;
-    Character new_character(character_name, static_cast<SPECIE>(SpecieInput -1), static_cast<CLASS>(ClassInput -1));
-    write_character_informations(new_character);
+    Character new_character(character_name, static_cast<SPECIE>(SpecieInput - 1), static_cast<CLASS>(ClassInput - 1), attr);
+    enter_character_informations(new_character);
+}
+int main()
+{
+    create_character();
 }
