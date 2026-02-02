edb8a7bb6ea224f01e2c6daf8b5a5319f6cb7704
Tobias Fajks <toby.fajks@gmail.com>
2026-02-02 22:44:51 +0100

    In process of implementation loading character option from a save slot for usage intended in near future

--- CharacterSheet.h
+++ CharacterSheet.h
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+enum MENU_ACTION { new_character, load_character, slot_select_1, slot_select_2, slot_select_3 };
 enum SPECIE {human, halfelf, elf, tiefling};
-enum CLASS {palladin, wizard};
+enum CLASS {barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, wizard};
 struct attributes
 {
 	int strength = 0;
@@ -28,10 +29,15 @@ public:
 		_specie = specie;
 		_class = char_class;
 		_attr = attr;
+		Get_Specie_Attribute_Bonus();
 	}
-	std::string getSpecie()
+	attributes Get_Attributes() 
 	{
-		switch (_specie)
+		return _attr;
+	}
+	void Get_Specie_Attribute_Bonus() 
+	{
+		switch (_specie) 
 		{
 		case human:
 			_attr.strength++;
@@ -40,52 +46,96 @@ public:
 			_attr.intelligence++;
 			_attr.wisdom++;
 			_attr.charisma++;
-			return "human";
 			break;
 		case halfelf:
 			_attr.charisma += 2;
 			free_attribute_points = 2;
-			return "half - elf";
 			break;
 		case elf:
-			_attr.dexterity+= 2;
-			return "elf";
+			_attr.dexterity += 2;
 			break;
 		case tiefling:
 			_attr.intelligence += 1;
 			_attr.charisma += 2;
+			break;
+		}
+	}
+	std::string Get_Specie()
+	{
+		switch (_specie)
+		{
+		case human:
+			return "human";
+			break;
+		case halfelf:
+			return "half - elf";
+			break;
+		case elf:
+			return "elf";
+			break;
+		case tiefling:
 			return "tiefling";
 			break;
 		}
 	}
-	std::string getClass() 
+	std::string Get_Class() 
 	{
 		switch (_class)
 		{
-		case palladin:
-			return "palladin";
+		case barbarian:
+			return "Barbarian";
+			break;
+		case bard:
+			return "Bard";
+			break;
+		case cleric:
+			return "Cleric";
+			break; 
+		case druid:
+			return "Druid";
+			break;
+		case fighter:
+			return "Fighter";
+			break;
+		case monk:
+			return "Monk";
+			break;
+		case paladin:
+			return "Paladin";
+			break;
+		case ranger:
+			return "Ranger";
+			break;
+		case rogue:
+			return "Rogue";
+			break;
+		case sorcerer:
+			return "Sorcerer";
+			break;
+		case warlock:
+			return "Warlock";
 			break;
 		case wizard:
-			return "wizard";
+			return "Wizard";
 			break;
 		}
 	}
 };
 int attribute_pick[6] = {};
-void getAttributes()
+void Get_Character_Attributes()
 {
 	srand(time(NULL));
 	for (int i = 0; i < 6; i++)
 	{
-		attribute_pick[i] = (rand() % 14) + 4;
+		attribute_pick[i] = (rand() % 14) + 3;
 		std::cout << "[" << i + 1 << "]" << " " << attribute_pick[i] << std::endl;
 	}
 }
 void enter_into_file(Character new_character, std::fstream &save) 
 {
 	save << new_character._name << std::endl;
-	save << new_character.getSpecie() << std::endl;
-	save << new_character.getClass() << std::endl;
+	save << new_character.Get_Specie() << std::endl;
+	save << new_character.Get_Class() << std::endl;
 	save << new_character._attr.strength << std::endl;
 	save << new_character._attr.dexterity << std::endl;
 	save << new_character._attr.constitution << std::endl;
@@ -93,22 +143,22 @@ void enter_into_file(Character new_character, std::fstream &save)
 	save << new_character._attr.wisdom << std::endl;
 	save << new_character._attr.charisma << std::endl;
 }
-void enter_character_save(int save_number, Character new_character) 
+void Enter_Character_Save(MENU_ACTION save_number, Character new_character) 
 {
 	std::ofstream save;
-	if (save_number == 1) 
+	if (save_number == slot_select_1) 
 	{
 		save.open("character_save_1.txt");
 		std::fstream save("character_save_1.txt", std::ios::app);
 		enter_into_file(new_character, save);
 	}
-	if (save_number == 2)
+	if (save_number == slot_select_2)
 	{
 		save.open("character_save_2.txt");
 		std::fstream save("character_save_2.txt", std::ios::app);
 		enter_into_file(new_character, save);
 	}
-	if (save_number == 3)
+	if (save_number == slot_select_3)
 	{
 		save.open("character_save_3.txt");
 		std::fstream save("character_save_3.txt", std::ios::app);
@@ -116,7 +166,7 @@ void enter_character_save(int save_number, Character new_character)
 	}
 	save.close();
 }
-std::string save_print(int save_number) 
+std::string Save_Print(int save_number) 
 {
 	std::string line = "";
 	std::fstream save_1_read("character_save_1.txt");
@@ -137,4 +187,99 @@ std::string save_print(int save_number)
 		return line;
 		break;
 	}
+}
+SPECIE Retrieve_Specie(std::string text) 
+{
+	if (text == "human")
+		return human;
+	else if (text == "half - elf")
+		return halfelf;
+	else if (text == "elf")
+		return elf;
+	else if (text == "tiefling")
+		return tiefling;
+}
+CLASS Retrieve_Class(std::string text)
+{
+	if (text == "Barbarian")
+		return barbarian;
+	else if (text == "Bard")
+		return bard;
+	else if (text == "Cleric")
+		return cleric;
+	else if (text == "Druid")
+		return druid;
+	else if (text == "Fighter")
+		return fighter;
+	else if (text == "Monk")
+		return monk;
+	else if (text == "Paladin")
+		return paladin;
+	else if (text == "Ranger")
+		return ranger;
+	else if (text == "Rogue")
+		return rogue;
+	else if (text == "Sorcerer")
+		return sorcerer;
+	else if (text == "Warlock")
+		return warlock;
+	else if(text == "Wizard")
+		return wizard;
+}
+Character Load_Text(std::fstream &save) 
+{
+	std::string name;
+	SPECIE specie;
+	CLASS character_class;
+	attributes attr;
+	int line_number = 0;
+	std::string line;
+	while (getline(save, line)) 
+	{
+		line_number++;
+		switch (line_number) 
+		{
+		case 1:
+			name = line;
+			break;
+		case 2:
+			specie = Retrieve_Specie(line);
+			break;
+		case 3:
+			character_class = Retrieve_Class(line);
+			break;
+		case 4:
+			attr.strength = stoi(line);
+			break;
+		case 5:
+			attr.dexterity = stoi(line);
+			break;
+		case 6:
+			attr.constitution = stoi(line);
+			break;
+		case 7:
+			attr.intelligence = stoi(line);
+			break;
+		case 8:
+			attr.wisdom = stoi(line);
+			break;
+		case 9:
+			attr.charisma = stoi(line);
+			break;
+		}
+	}
+	Character Current(name, static_cast<SPECIE>(specie), static_cast<CLASS>(character_class), attr);
+	return Current;
+}
+Character Save_Slot_Selection_Load(MENU_ACTION character_selection_slot) 
+{
+	std::fstream save_1("character_save_1.txt");
+	std::fstream save_2("character_save_2.txt");
+	std::fstream save_3("character_save_3.txt");
+	if (character_selection_slot == slot_select_1)
+		return Load_Text(save_1);
+	else if (character_selection_slot == slot_select_2)
+		return Load_Text(save_2);
+	else if (character_selection_slot == slot_select_3)
+		return Load_Text(save_3);
 }
--- DnD.cpp
+++ DnD.cpp
@@ -2,84 +2,93 @@
 #include <string>
 #include <cstdlib>
 #include "CharacterSheet.h"
-int starting_menu() 
+MENU_ACTION Starting_Menu() 
 {
-    int UsersChoice;
+    int users_choice;
     std::cout << "0*------------------*0\n";
     std::cout << "I  [1]New Character  I\n";
-    std::cin >> UsersChoice;
-    return UsersChoice;
+    std::cout << "I  [2]Load Character I\n";
+    std::cin >> users_choice;
+    return static_cast<MENU_ACTION>(users_choice);
 }
-int available_saves() 
+MENU_ACTION Available_Saves() 
 {
-    int UsersChoice;
-    std::cout << "save slot 1: " << save_print(1) << std::endl;
-    std::cout << "save slot 2: " << save_print(2) << std::endl;
-    std::cout << "save slot 3: " << save_print(3) << std::endl;
+    int users_choice;
+    std::cout << "save slot 1: " << Save_Print(1) << std::endl;
+    std::cout << "save slot 2: " << Save_Print(2) << std::endl;
+    std::cout << "save slot 3: " << Save_Print(3) << std::endl;
     std::cout << "Select slot to save character in" << std::endl;
-    std::cin >> UsersChoice;
-    return UsersChoice;
+    std::cin >> users_choice;
+    return static_cast<MENU_ACTION>(users_choice);
 }
-void create_character() 
+void Create_Character() 
 {
     std::cin.ignore();
     attributes attr;
     std::string character_name;
-    int SpecieInput;
-    int AttrInput;
-    int ClassInput;
+    int specie_input;
+    int attribute_input;
+    int class_input;
     std::cout << "enter character's name:\n";
     getline(std::cin, character_name);
     std::cout << "[1]human\n[2]half-elf\n[3]elf\n[4]tiefling\n";
     std::cout << "what's your character's race?\n";
-    std::cin >> SpecieInput;
-    getAttributes();
+    std::cin >> specie_input;
+    std::cout << "[1]Barbarian\n[2]Bard\n[3]Cleric\n[4]Druid\n[5]Fighter\n[6]Monk\n[7]Paladin\n[8]Ranger\n[9]Rogue\n[10]Sorcerer\n[11]Warlock\n[12]Wizard\n";
+    std::cout << "what's your character's class?\n";
+    std::cin >> class_input;
+    Get_Character_Attributes();
     for (int i = 0; i < 6; i++) 
     {
         switch (i) 
         {
         case 0:
             std::cout << "pick number for Strength: ";
-            std::cin >> AttrInput;
-            attr.strength += attribute_pick[AttrInput-1];
+            std::cin >> attribute_input;
+            attr.strength += attribute_pick[attribute_input -1];
             break;
         case 1:
             std::cout << "pick number for Dexterity: ";
-            std::cin >> AttrInput;
-            attr.dexterity += attribute_pick[AttrInput-1];
+            std::cin >> attribute_input;
+            attr.dexterity += attribute_pick[attribute_input -1];
             break;
         case 2:
             std::cout << "pick number for Constitution: ";
-            std::cin >> AttrInput;
-            attr.constitution += attribute_pick[AttrInput-1];
+            std::cin >> attribute_input;
+            attr.constitution += attribute_pick[attribute_input -1];
             break;
         case 3:
             std::cout << "pick number for Intelligence: ";
-            std::cin >> AttrInput;
-            attr.intelligence += attribute_pick[AttrInput-1];
+            std::cin >> attribute_input;
+            attr.intelligence += attribute_pick[attribute_input -1];
             break;
         case 4:
             std::cout << "pick number for Wisdom: ";
-            std::cin >> AttrInput;
-            attr.wisdom += attribute_pick[AttrInput-1];
+            std::cin >> attribute_input;
+            attr.wisdom += attribute_pick[attribute_input -1];
             break;
         case 5:
             std::cout << "pick number for Charisma: ";
-            std::cin >> AttrInput;
-            attr.charisma += attribute_pick[AttrInput-1];
+            std::cin >> attribute_input;
+            attr.charisma += attribute_pick[attribute_input -1];
             break;
         }
     }
-    std::cout << "[1]palladin\n[2]wizard\n";
-    std::cout << "what's your character's class?\n";
-    std::cin >> ClassInput;
-    Character new_character(character_name, static_cast<SPECIE>(SpecieInput - 1), static_cast<CLASS>(ClassInput - 1), attr);
-    int save_slot_choice = available_saves();
-    enter_character_save(save_slot_choice, new_character);
+    Character new_character(character_name, static_cast<SPECIE>(specie_input - 1), static_cast<CLASS>(class_input - 1), attr);
+    MENU_ACTION save_slot_choice = Available_Saves();
+    Enter_Character_Save(save_slot_choice, new_character);
+}
+void Load_Saved_Character() 
+{
+    std::cout << "Select character to use:\n";
+    MENU_ACTION slot_number = Available_Saves();
+    Save_Slot_Selection_Load(slot_number);
 }
 int main()
 {
-    int menu_selection = starting_menu();
-    if (menu_selection == 1)
-        create_character();
+    MENU_ACTION menu_selection = Starting_Menu();
+    if (menu_selection == new_character)
+        Create_Character();
+    else if (menu_selection == load_character)
+        Load_Saved_Character();
 }
